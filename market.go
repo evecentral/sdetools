49a9d53425091ae576cf5184075414ae60f22336
Yann Ramin <atrus@stackworks.net>
2016-11-17 04:07:46 +0000

    Add a group deserializer

--- market.go
+++ market.go
@@ -3,3 +3,24 @@ package sdetools
 import (
 	"gopkg.in/yaml.v2"
 )
+
+type GroupId struct {
+	Anchorable bool `yaml:"anchorable"`
+	Anchored bool `yaml:"anchored"`
+	CategoryId int `yaml:"categoryID"`
+	FittableNonSingleton bool `yaml:"fittableNonSingleton"`
+	Name map[string]string `yaml:"name"`
+	Published bool `yaml:"published"`
+	UseBasePrice bool `yaml:"useBasePrice"`
+}
+
+type Groups map[int]GroupId
+
+func LoadGroups(data []byte) (*Groups, error) {
+	var group Groups
+	err := yaml.Unmarshal(data, &group)
+	if err != nil {
+		return nil, err
+	}
+	return &group, nil
+}
